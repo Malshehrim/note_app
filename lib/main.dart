c295527c5d52273db99b5263ab6ad40bd284a014
Msh <gigu930@gmail.com>
2024-07-28 13:14:18 +0300

    add folders and screens

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/screens/note_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: NoteScreen(),
+    ),
+  );
+}
